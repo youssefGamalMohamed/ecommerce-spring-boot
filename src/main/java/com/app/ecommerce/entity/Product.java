9fb0b939e479351dea71b46b869331227b8f73d5
Youssef Gamal <youssef.gamal@intercom.com.eg>
2023-05-21 13:56:08 +0300

    Removed unnecessary codes like writing my own hashCode() in Product

--- src/main/java/com/app/ecommerce/entity/Product.java
+++ src/main/java/com/app/ecommerce/entity/Product.java
@@ -51,18 +51,7 @@ public class Product {
     @JsonIgnore
     private List<CartItem> cartItem;
     
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || Hibernate.getClass(this) != Hibernate.getClass(o)) return false;
-        Product product = (Product) o;
-        return id != null && Objects.equals(id, product.id);
-    }
 
-    @Override
-    public int hashCode() {
-        return getClass().hashCode();
-    }
 
 	@Override
 	public String toString() {
