775a57cac2b57faa199696829c544df8d21e37d2
Youssef Gamal <youssef.gamal@intercom.com.eg>
2023-05-23 22:03:20 +0300

    Remove Un-necessary code from SecurityConfiguration

--- src/main/java/com/app/ecommerce/config/SecurityConfiguration.java
+++ src/main/java/com/app/ecommerce/config/SecurityConfiguration.java
@@ -46,10 +46,7 @@ public class SecurityConfiguration {
                 .and()
                 .authenticationProvider(authenticationProvider)
                 .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class)
-                .logout()
-                .logoutUrl("/auth/logout")
-                .addLogoutHandler(logoutHandler)
-                .logoutSuccessHandler((request, response, authentication) -> SecurityContextHolder.clearContext())
+
         ;
 
         return http.build();
