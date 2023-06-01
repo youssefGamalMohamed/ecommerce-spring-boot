2191f75b19be631980308878d9cb975701c156aa
Youssef Gamal <youssef.gamal@intercom.com.eg>
2023-06-02 01:02:01 +0300

    Refactor code for corner cases

--- src/main/java/com/app/ecommerce/logging/LoggingUtils.java
+++ src/main/java/com/app/ecommerce/logging/LoggingUtils.java
@@ -2,13 +2,13 @@ package com.app.ecommerce.logging;
 
 import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Component;
-import org.thymeleaf.util.StringUtils;
+
 
 @Component
 @Log4j2
 public class LoggingUtils {
 
     public void logSeparator(){
-        log.info(StringUtils.repeat("=" , 200).toString());
+        log.info("=".repeat(200));
     }
 }
--- src/main/java/com/app/ecommerce/security/handler/CustomLogoutHandler.java
+++ src/main/java/com/app/ecommerce/security/handler/CustomLogoutHandler.java
@@ -4,6 +4,7 @@ import com.app.ecommerce.repository.TokenRepo;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.web.authentication.logout.LogoutHandler;
@@ -23,11 +24,14 @@ public class CustomLogoutHandler implements LogoutHandler {
         jwt = authHeader.substring(7);
         var storedToken = tokenRepo.findByToken(jwt)
                 .orElse(null);
-        if (storedToken != null) {
-            storedToken.setExpired(true);
-            storedToken.setRevoked(true);
-            tokenRepo.save(storedToken);
-            SecurityContextHolder.clearContext();
-        }
+
+        if(storedToken == null)
+            throw new BadCredentialsException("token not exist to logout from system");
+
+        storedToken.setExpired(true);
+        storedToken.setRevoked(true);
+        tokenRepo.save(storedToken);
+        SecurityContextHolder.clearContext();
+
     }
 }
