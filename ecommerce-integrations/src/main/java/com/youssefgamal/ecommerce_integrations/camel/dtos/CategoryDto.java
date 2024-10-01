36a037aafdc365a2985e5f005ef592b1ca0a2926
Youssef Gamal <Youssef.Gamal@intercom.com.eg>
2024-10-01 16:28:45 +0300

    removing wanted imports

--- ecommerce-integrations/src/main/java/com/youssefgamal/ecommerce_integrations/camel/dtos/CategoryDto.java
+++ ecommerce-integrations/src/main/java/com/youssefgamal/ecommerce_integrations/camel/dtos/CategoryDto.java
@@ -4,8 +4,6 @@ package com.youssefgamal.ecommerce_integrations.camel.dtos;
 import com.fasterxml.jackson.annotation.JsonInclude;
 
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotEmpty;
-import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
