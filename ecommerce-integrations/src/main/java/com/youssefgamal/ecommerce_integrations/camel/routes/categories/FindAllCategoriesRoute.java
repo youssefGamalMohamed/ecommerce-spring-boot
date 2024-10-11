4957ca4d56211377fbd2e1e560fac7c31d90ed0a
Youssef Gamal <Youssef.Gamal@intercom.com.eg>
2024-10-11 19:32:40 +0300

    applying caching using redis into categories entpoint

--- ecommerce-integrations/src/main/java/com/youssefgamal/ecommerce_integrations/camel/routes/categories/FindAllCategoriesRoute.java
+++ ecommerce-integrations/src/main/java/com/youssefgamal/ecommerce_integrations/camel/routes/categories/FindAllCategoriesRoute.java
@@ -1,7 +1,11 @@
 package com.youssefgamal.ecommerce_integrations.camel.routes.categories;
 
+import java.util.List;
+import java.util.Map;
+
 import org.apache.camel.Exchange;
 import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.model.dataformat.JsonLibrary;
 import org.springframework.http.HttpMethod;
 import org.springframework.stereotype.Component;
 
@@ -16,13 +20,33 @@ public class FindAllCategoriesRoute extends RouteBuilder {
 	    .doTry()
 	        .routeId("FindAllCategoriesRoute")
 	        .to("direct:LogRequestRoute")
-	        .log("Before Calling Backend ==> BODY = ${body} , HEADERS = ${headers}")
-	        .setHeader(Exchange.HTTP_METHOD, constant(HttpMethod.GET))
-	        .toD("{{ecommerce.category-service.url}}?bridgeEndpoint=true")
-	        .log("After Save Category ==> ${headers.id} ==> BODY = ${body} , HEADERS = ${headers}")
-	        .to("direct:LogResponseRoute")
+            .setHeader("CamelRedis.Key", simple("CATEGORIES_CACHE::ALL"))
+            .to("spring-redis://localhost:6379?command=GET")
+	        .choice()
+	        	.when(exchange -> exchange.getIn().getBody() == null || exchange.getIn().getBody(String.class).isBlank() ||  exchange.getIn().getBody(String.class).isEmpty())
+	        		.log("Cache: No Value Present for key = ${headers.CamelRedis.Key}, body = ${body}")
+	    	        .log("Before Calling Backend ==> BODY = ${body} , HEADERS = ${headers}")
+	    	        .setHeader(Exchange.HTTP_METHOD, constant(HttpMethod.GET))
+	    	        .toD("{{ecommerce.category-service.url}}?bridgeEndpoint=true")
+	    	        .log("After Save Category ==> ${headers.id} ==> BODY = ${body} , HEADERS = ${headers}")
+	    	        .setHeader("CamelRedis.Key", simple("CATEGORIES_CACHE::ALL"))
+	    	        .setHeader("CamelRedis.Value", simple("${bodyAs(java.lang.String)}"))
+                    .setHeader("CamelRedis.Timeout", constant(10)) // set value with expiration time in seconds
+                    .to("spring-redis://localhost:6379?command=SETEX") 
+                    .log("Cache: Saved into Redis: SETEX key = ${headers.CamelRedis.Key} with value = ${headers.CamelRedis.Value} and expiration of ${headers.CamelRedis.Timeout} seconds")
+                    .unmarshal().json(List.class)
+                    .marshal().json(JsonLibrary.Jackson)
+                    .endChoice()
+	        	.otherwise()
+	        		.log("Cache Hit Values Exist, body = ${body}")
+	        		.unmarshal().json(List.class)
+                    .marshal().json(JsonLibrary.Jackson)
+                    .endChoice()
+	        .endDoTry()
 	    .doCatch(Exception.class)
 	    	.to("direct:RestExceptionHandlingRoute")
+	    .doFinally()
+	    	.to("direct:LogResponseRoute")
 	    .end()
 	    ;
 	}
--- ecommerce-integrations/src/main/java/com/youssefgamal/ecommerce_integrations/camel/routes/categories/FindCategoryByIdRoute.java
+++ ecommerce-integrations/src/main/java/com/youssefgamal/ecommerce_integrations/camel/routes/categories/FindCategoryByIdRoute.java
@@ -1,7 +1,10 @@
 package com.youssefgamal.ecommerce_integrations.camel.routes.categories;
 
+import java.util.Map;
+
 import org.apache.camel.Exchange;
 import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.model.dataformat.JsonLibrary;
 import org.springframework.http.HttpMethod;
 import org.springframework.stereotype.Component;
 
@@ -13,17 +16,36 @@ public class FindCategoryByIdRoute extends RouteBuilder {
 	@Override
 	public void configure() throws Exception {
 		from("direct:FindCategoryByIdRoute")
-	    .doTry()
-	        .routeId("FindCategoryByIdRoute")
-	        .to("direct:LogRequestRoute")
-	        .log("Before Calling Backend ==> BODY = ${body} , HEADERS = ${headers}")
-	        .setHeader(Exchange.HTTP_METHOD, constant(HttpMethod.GET))
-	        .toD("{{ecommerce.category-service.url}}/${header.id}?bridgeEndpoint=true")
-		    .log("Category GET /categories/${headers.id} Successfully with id = ${headers.id}")
-	    .doCatch(Exception.class)
-	    	.to("direct:RestExceptionHandlingRoute")
-	    .end()
-	    ;
+        .doTry()
+            .routeId("FindCategoryByIdRoute")
+            .to("direct:LogRequestRoute")
+            .setHeader("CamelRedis.Key", simple("CATEGORIES_CACHE::${headers.id}"))
+            .to("spring-redis://localhost:6379?command=GET")
+            .choice()
+                .when(body().isEqualTo(""))
+                    .log("REDIS GET returned null for key: ${headers.CamelRedis.Key}")
+                    .log("Before Calling Backend ==> BODY = ${body} , HEADERS = ${headers}")
+                    .setHeader(Exchange.HTTP_METHOD, constant(HttpMethod.GET))
+                    .toD("{{ecommerce.category-service.url}}/${header.id}?bridgeEndpoint=true")
+                    .log("Category GET /categories/${headers.id} Successfully with id = ${headers.id}")
+                    .setHeader("CamelRedis.Value", simple("${bodyAs(java.lang.String)}"))
+                    .setHeader("CamelRedis.Timeout", constant(10)) // set value with expiration time in seconds
+                    .to("spring-redis://localhost:6379?command=SETEX") 
+                    .log("REDIS SETEX key = ${headers.CamelRedis.Key} with value = ${headers.CamelRedis.Value} and expiration of 3600 seconds")
+                    .unmarshal().json(Map.class)
+                    .marshal().json(JsonLibrary.Jackson)
+                .endChoice()
+            .otherwise()
+                .log("REDIS GET result for key: ${headers.CamelRedis.Key} is ${body}")
+                .unmarshal().json(Map.class)
+                .marshal().json(JsonLibrary.Jackson)
+        .endDoTry()
+        .doCatch(Exception.class)
+            .to("direct:RestExceptionHandlingRoute")
+        .doFinally()
+            .to("direct:LogResponseRoute")
+        .end();
+		
 	}
-
+	
 }
