0bb5306024b5e25fb9283dfc48fd7b064db79cbb
Youssef Gamal <Youssef.Gamal@intercom.com.eg>
2024-10-26 23:10:18 +0300

    simplify db relations for cart service

--- cart-service/src/main/java/com/youssefgamal/cart_service/entity/CartItem.java
+++ cart-service/src/main/java/com/youssefgamal/cart_service/entity/CartItem.java
@@ -1,14 +1,11 @@
 package com.youssefgamal.cart_service.entity;
 
-import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
-import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
-import jakarta.persistence.OneToOne;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -37,6 +34,5 @@ public class CartItem extends BaseEntity {
 	private Cart cart;
 	
 	
-	@OneToOne(mappedBy = "cartItem", cascade = CascadeType.ALL, fetch = FetchType.EAGER)
-	private Product product;
+	private Long productId;
 }
